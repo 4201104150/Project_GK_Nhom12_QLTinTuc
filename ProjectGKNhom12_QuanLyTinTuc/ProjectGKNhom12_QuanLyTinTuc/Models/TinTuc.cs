6653a8e68c21a813345bf2319a0b0764cc35cc0d
quynh1998 <37840050+quynh1998@users.noreply.github.com>
2018-12-01 13:36:48 +0700

    Upfile_TinTuc

---
+++ ProjectGKNhom12_QuanLyTinTuc/ProjectGKNhom12_QuanLyTinTuc/Models/TinTuc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectGKNhom12_QuanLiTinTuc.Models
+{
+    public class TinTuc
+    {
+        [Key]
+        public string MaTin { get; set; }
+        public string TieuDe { get; set; }
+        public string NoiDung { get; set; }
+        public string Anh { get; set; }
+        public DateTime NgayDang { get; set; }
+        public string NguoiDang { get; set; }
+        [ForeignKey("MaTL")]
+        public string MaTheLoai { get; set; }
+    }
+}
