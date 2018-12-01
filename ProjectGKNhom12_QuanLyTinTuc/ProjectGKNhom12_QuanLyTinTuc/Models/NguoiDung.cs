c5ac75f433793b6f884b32c87d88d7c22f973b3a
lamns81297 <44387281+lamns81297@users.noreply.github.com>
2018-12-01 13:36:04 +0700

    Add files via upload
    class NguoiDung

---
+++ ProjectGKNhom12_QuanLyTinTuc/ProjectGKNhom12_QuanLyTinTuc/Models/NguoiDung.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectGKNhom12_QuanLiTinTuc.Models
+{
+    public class NguoiDung
+    {
+        [Key]
+        public string MaNguoiDung { get; set; }
+        public string HoTen { get; set; }
+        public string MatKhau { get; set; }
+        public int KieuNguoiDung { get; set; }
+    }
+}
