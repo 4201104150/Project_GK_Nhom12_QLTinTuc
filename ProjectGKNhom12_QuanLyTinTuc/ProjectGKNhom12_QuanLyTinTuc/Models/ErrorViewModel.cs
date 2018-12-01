a02b3fa6f8bff5fef543febdd8f521bf364a27c1
DESKTOP-00HTOC2\Escanor <shenlong436@gmail.com>
2018-12-01 13:20:43 +0700

    createproject

---
+++ ProjectGKNhom12_QuanLyTinTuc/ProjectGKNhom12_QuanLyTinTuc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectGKNhom12_QuanLyTinTuc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
